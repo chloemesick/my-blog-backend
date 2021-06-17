788f4f8e3015075fb6d6fee0690aac6d2702379d
Chloe Mesick <cmesick@colum.edu>
2021-06-16 21:44:05 -0400

    First Commit

---
+++ .history/src/server_20210616143031.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/hello', (req, res) => res.send('Hello!'));
+app.post()
+
+app.listen(8000, () => console.log('Listening on port 8000'));
+
