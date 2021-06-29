1c9ba7b1206343b9682460908cadf9dd3ec5906e
Chloe Mesick <cmesick@colum.edu>
2021-06-28 22:40:02 -0400

    Both repositories in one place now-6-28

---
+++ .history/src/server_20210626120159.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyparser.json());
+
+app.get('/hello', (req, res) => res.send('Hello!'));
+app.post('/hello', (req, res) => res.send(~));
+
+app.listen(8000, () => console.log('Listening on port 8000'));
+
